7c71e331a648272f86250903741125ddfac8a890
Mariela <111813910+Mariela-1@users.noreply.github.com>
2024-11-02 21:34:33 -0500

    setting

---
+++ src/app/StockFlow/setting/setting.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-setting',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './setting.component.html',
+  styleUrls: ['./setting.component.scss']
+})
+export class SettingComponent {
+
+}
